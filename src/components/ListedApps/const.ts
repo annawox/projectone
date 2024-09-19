75915c9aae6056a8b326d3c8b70a431c071fec4a
anna <143606587+annawox@users.noreply.github.com>
2024-09-19 05:31:55 +0300

    Add files via upload

---
+++ src/components/ListedApps/const.ts
@@ -0,0 +1,23 @@
+import spotify from '../../assets/images/apps/spotify.svg';
+import pintrest from '../../assets/images/apps/pintrest.svg';
+import soundcloud from '../../assets/images/apps/soundcloud.svg';
+import app1 from '../../assets/images/apps/app1.svg';
+import app2 from '../../assets/images/apps/app2.svg';
+
+
+export const LISTED_APPS = [{
+    url: '#',
+    icon: spotify
+}, {
+    url: '#',
+    icon: pintrest
+}, {
+    url: '#',
+    icon: soundcloud
+}, {
+    url: '#',
+    icon: app1
+}, {
+    url: '#',
+    icon: app2
+}]
